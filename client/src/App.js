eea60492549d9dfd35be115cab23804da07718ef
michael <mmansourati@gmail.com>
2018-08-15 09:41:54 -0400

    making GET requests in react app, rendering a list

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
   state = {
-    response: ''
+    // response: ''
+    type: '',
+    list: []
   };
 
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({response: res.express}))
-      .catch(err => console.log(err));
+    // this.callApi()
+    //   .then(res => this.setState({response: res.express}))
+    //   .catch(err => console.log(err));
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
+  fetchData = type => {
+    console.log('hello fetchData')
+    return axios
+      .get(`https://swapi.co/api/${type}`)
+      .then(response => {
+        this.setState({list: response.data.results})
+      })
   }
+
+  handleButtonClick = (type, e) => {
+    this.setState({type});
+    this.fetchData(type)
+  }
+
+
+  // callApi = async () => {
+  //   const response = await fetch('/api/hello');
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) throw Error(body.message);
+
+  //   return body;
+  // }
 
   render() {
+    console.log(this.state)
+    const list = this.state.list.length ? this.state.list.map(item => {
+      return <li key={item.url} >{item.name}</li>
+    })
+    : ''
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Modal Nodes JS</h1>
+          <p>
+            <button onClick={(e) => this.handleButtonClick('people', e)}>
+              People
+            </button>
+            <button onClick={(e) => this.handleButtonClick('planets', e)}>
+              Planets
+            </button>
+            <button onClick={(e) => this.handleButtonClick('starships', e)}>
+              Starships
+            </button>
+          </p>
         </header>
-        <p className="App-intro">
-          {this.state.response}
-        </p>
+        <h3 className="App-intro">
+          {this.state.type}
+        </h3>
+        <ul style={{listStyle: 'none', padding: 0}}>
+          {list}
+        </ul>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+// app.get('/people', (req, res) => {
+//   request('https://swapi.co/api/people', (err, res, body) => {
+//     if (res.status === 200){
+//       console.log(res);
+//       res.send({body});
+//     } else {
+//       console.log(err)
+//     }
+//   })
+// })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
