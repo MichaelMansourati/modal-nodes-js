934472ad0967dad7b18b954c3a25e905762251f5
michael <mmansourati@gmail.com>
2018-08-15 23:00:46 -0400

    GET to /people returns all people, not just page1 of request. submit handler passes params to express

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+// import axios from 'axios'
 import './App.css';
 
 class App extends Component {
   state = {
     // response: ''
     type: '',
-    list: []
-  };
+    list: [],
+    name: ''
+  }
 
   componentDidMount() {
     // this.callApi()
@@ -15,18 +16,47 @@ class App extends Component {
     //   .catch(err => console.log(err));
   }
 
-  fetchData = type => {
-    console.log('hello fetchData')
-    return axios
-      .get(`https://swapi.co/api/${type}`)
-      .then(response => {
-        this.setState({list: response.data.results})
-      })
+  // fetchData = type => {
+  //   console.log('hello fetchData')
+  //   return axios
+  //     .get(`https://swapi.co/api/${type}`)
+  //     .then(response => {
+  //       this.setState({list: response.data.results})
+  //     })
+  // }
+
+  handleChange = e => {
+    this.setState({ name: e.target.value })
   }
 
-  handleButtonClick = (type, e) => {
-    this.setState({type});
-    this.fetchData(type)
+  handleSubmit = async e => {
+    e.preventDefault()
+    console.log(e.target)
+    const searchResponse = await fetch(`/people/${this.state.name}`)
+    const searchList = await searchResponse.json()
+    //this.setState({ name: '' })
+  }
+
+  handleButtonClick = async (type, e) => {
+    switch (type) {
+    case 'people':
+      const peopleResponse = await fetch('/people');
+      const peopleList = await peopleResponse.json();
+      this.setState({list: peopleList.list})
+      break
+    case 'planets':
+      const planetsResponse = await fetch('/planets');
+      const planetsList = await planetsResponse.json();
+      this.setState({list: planetsList.list})
+      break
+    case 'starships':
+      const starshipsResponse = await fetch('/starships');
+      const starshipsList = await starshipsResponse.json();
+      this.setState({list: starshipsList.list})
+      break
+    default:
+      break
+    }
   }
 
 
@@ -40,7 +70,6 @@ class App extends Component {
   // }
 
   render() {
-    console.log(this.state)
     const list = this.state.list.length ? this.state.list.map(item => {
       return <li key={item.url} >{item.name}</li>
     })
@@ -61,6 +90,12 @@ class App extends Component {
               Starships
             </button>
           </p>
+          <form onSubmit={this.handleSubmit}>
+            <span>search people: </span>
+            <input type="text" onChange={this.handleChange} value={this.state.name} />
+            <span> </span>
+            <input type="submit" value="search" />
+          </form>
         </header>
         <h3 className="App-intro">
           {this.state.type}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const axios   = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,15 +9,50 @@ app.get('/', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
-// app.get('/people', (req, res) => {
-//   request('https://swapi.co/api/people', (err, res, body) => {
-//     if (res.status === 200){
-//       console.log(res);
-//       res.send({body});
-//     } else {
-//       console.log(err)
-//     }
-//   })
-// })
+app.get('/people/:name', (req, res) => {
+  console.log(req.params)
+})
+
+app.get('/people', (req, res) => {
+  let list = []
+  let pages = null
+  axios
+    .get(`https://swapi.co/api/people`)
+    .then(async response => {
+      pages = Math.ceil(response.data.count/10);
+      for(let i = 1; i <= pages; i++){
+        await axios
+          .get(`https://swapi.co/api/people/?page=${i}`)
+          .then(pageResponse => {
+            pageResponse.data.results.forEach(item => {
+              list.push(item);
+            })
+          })
+      }
+    })
+    .then(() => {
+    console.log(pages)
+    res.send({list, pages}) //sending empty array!!!
+    })
+})
+
+app.get('/planets', (req, res) => {
+  axios
+    .get(`https://swapi.co/api/planets`)
+    .then(response => {
+      res.send({list: response.data.results})
+    })
+})
+
+app.get('/starships', (req, res) => {
+  axios
+    .get(`https://swapi.co/api/starships`)
+    .then(response => {
+      res.send({list: response.data.results})
+    })
+})
+
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
